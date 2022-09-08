1869738b8c990eb67fbbd2adc6c0e3ca760d45c6
Benjamin Schwartz <bs4308@nyu.edu>
2022-09-08 16:15:17 -0400

    Write absolutely absurd bubble sort

--- intro_callbacks.js
+++ intro_callbacks.js
@@ -33,11 +33,11 @@ class Clock {
 
 // const clock = new Clock();
 
-// const readline = require('node:readline');
-// reader = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const readline = require('node:readline');
+reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 function addNumbers(sum, numsLeft, completionCallback) {
   let parsed;
@@ -80,5 +80,57 @@ const lamp = new Lamp();
 
 // boundTurnOn(); // should say "Turning on a lamp"
 // myBoundTurnOn(); // should say "Turning on a lamp"
+
+
+const absurdBubbleSort = (arr, sortCompletionCallback) => {
+  const outerBubbleSortLoop = (madeAnySwaps) => {
+    if (madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+  outerBubbleSortLoop(true);
+}
+
+const askIfGreaterThan = (el1, el2, callback) => {
+  reader.question(`Is ${el1} greater than ${el2}?? Yes or No.`, response => {
+    if (response === 'Yes'){
+      callback(true);
+    } else if (response === 'No') {
+      callback(false);
+    } else {
+      console.log("Please enter 'Yes' or 'No', OK??")
+    }
+    // reader.close()
+  })
+}
+
+const innerBubbleSortLoop = (arr, i, madeAnySwaps, outerBubbleSortLoop) => {
+  if (i < arr.length - 1){
+    askIfGreaterThan(arr[i], arr[i+1], function(bool) {
+      if (bool) {
+        const next = arr[i+1];
+        arr[i+1] = arr[i];
+        arr[i] = next;
+        madeAnySwaps = true;
+      } else {
+        madeAnySwaps = false;
+      }
+      innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  } else if (i === (arr.length- 1)) {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+absurdBubbleSort([3, 2, 1], function(arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+});
+
+
+
+
 
 
