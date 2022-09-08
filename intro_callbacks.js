408c334054711a423a534652f98dc0dc8302b9e5
Benjamin Schwartz <bs4308@nyu.edu>
2022-09-08 15:23:21 -0400

    Work on mybind

--- intro_callbacks.js
+++ intro_callbacks.js
@@ -33,11 +33,11 @@ class Clock {
 
 // const clock = new Clock();
 
-const readline = require('node:readline');
-reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('node:readline');
+// reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 function addNumbers(sum, numsLeft, completionCallback) {
   let parsed;
@@ -48,13 +48,37 @@ function addNumbers(sum, numsLeft, completionCallback) {
       console.log(sum);
       numsLeft--;
       addNumbers(sum, numsLeft, completionCallback)
-      // reader.close();
     });
   } else if (numsLeft === 0) {
     reader.close();
-    // console.log(`finished! ${sum}`)
     completionCallback(sum);
   }
 }
 
 // addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+Function.prototype.myBind = function (context) {
+  return this.apply(context);
+}
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function() {
+  console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+// turnOn(); // should not work the way we want it to
+
+// const boundTurnOn = turnOn.bind(lamp);
+// const myBoundTurnOn = turnOn.myBind(lamp);
+
+// boundTurnOn(); // should say "Turning on a lamp"
+// myBoundTurnOn(); // should say "Turning on a lamp"
+
+
